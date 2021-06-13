f981667946834408a6742b41f4e3e9afbc460214
Niklas Braun <NiklasBraun93@googlemail.com>
2021-06-13 19:11:22 +0200

    Jio das Drehspiel halt

--- UnityProject/Assets/Scripts/Games/AlignGame.cs
+++ UnityProject/Assets/Scripts/Games/AlignGame.cs
@@ -11,30 +11,93 @@ namespace SaARbotage
         public GameObject leftZahnrad;
 
         public GameObject innerRing;
+        private Vector3 innerRingUp;
+        private Transform InnerEmpty;
         public GameObject outerRing;
+        private Vector3 outerRingUP;
+        private Transform OuterEmpty;
+
+       
+
+        private float targetdistance;
 
         public GameObject mesh;
 
+        public Material Indicator;
+
         public float RingRotationSpeed = 1f;
         public float RotationSpeed = 20;
+
+        public float FairnessThreshold = 5f;
+
+        private float OffsetAngl;
+
+        private void Awake()
+        {
+            InnerEmpty = innerRing.transform.GetChild(0).transform;
+            OuterEmpty = outerRing.transform.GetChild(0).transform;
+            SetUpGame();
+        }
+
+        // Also hier werden die Winkel erstmals verstellt, dass es replayable is.
+        private void SetUpGame()
+        {
+            OffsetAngl = UnityEngine.Random.Range(1, 359);
+            innerRing.transform.Rotate(new Vector3(OffsetAngl, OffsetAngl, OffsetAngl));
+            innerRingUp = innerRing.transform.up;
+            outerRing.transform.Rotate(new Vector3(OffsetAngl, OffsetAngl, OffsetAngl));
+            outerRingUP = outerRing.transform.up;
+            Debug.Log("Lösung " + innerRing.transform.rotation.ToString() + " and " + outerRing.transform.rotation.ToString());
+
+            targetdistance = Vector3.Distance(InnerEmpty.position, OuterEmpty.position);
+            Debug.Log(targetdistance);
+            
+
+            float OffsetAngle1 = UnityEngine.Random.Range(45,360);
+            //float OffsetAngle2 = UnityEngine.Random.Range(45, 360);
+            innerRing.transform.Rotate(new Vector3 (OffsetAngle1, OffsetAngle1, 0));
+            outerRing.transform.Rotate(new Vector3(0, OffsetAngle1, OffsetAngle1));
+            Indicator.SetColor("_EmissionColor", new Color (255f, 0f, 0f));
+
+        }
 
         private void Update()
         {
             //if (!launch) return;
             //if(!IsLocalPlayer) return;   
-
-            // Rotiere Ringe jenachdem wie Z jetzt steht bei Dingens.
+            if(Input.GetMouseButtonUp(0)) TestAlignment();
         }
 
         public void RotateRing(GameObject ring, float prog)
         {
             if (ring == innerRing) {
-                innerRing.transform.Rotate(new Vector3(prog * RingRotationSpeed, prog * RingRotationSpeed, 0f));
-                } else
+                //Lösung aus: https://forum.unity.com/threads/two-rotatearound-calls-lead-to-3-axis-rotation.362943/ ABER is ehrlich gesagt nicht so geil tbh...
+                innerRing.transform.RotateAround(innerRing.transform.position, innerRingUp, prog * RingRotationSpeed);
+                innerRing.transform.RotateAround(innerRing.transform.position, innerRing.transform.right, prog * RingRotationSpeed);
+            } else
                 {
-                    outerRing.transform.Rotate(new Vector3(0f, prog * RingRotationSpeed, prog * RingRotationSpeed));
+                outerRing.transform.RotateAround(outerRing.transform.position, outerRingUP, prog * RingRotationSpeed);
+                outerRing.transform.RotateAround(outerRing.transform.position, outerRing.transform.forward, prog * RingRotationSpeed);
             }
 
+        }
+
+        private void TestAlignment()
+        {
+
+            Debug.Log(Vector3.Distance(InnerEmpty.position, OuterEmpty.position));
+            if (Vector3.Distance(InnerEmpty.position, OuterEmpty.position) <= targetdistance + FairnessThreshold/100) EndGame();
+            /*//Problem: Manchmal tackt das beim rotieren doch noch die angles slighty. Also wird die achse die bspw. nicht rotiert werden soll auf einmal -179,999 anstatt 0 bspw. Das könnte problematisch werden..
+            Debug.Log(Quaternion.Angle(innerRing.transform.rotation, outerRing.transform.rotation).ToString());
+            if (Quaternion.Angle(innerRing.transform.rotation, outerRing.transform.rotation) <= FairnessThreshold)
+            {
+                EndGame(); } */
+        }
+
+        private void EndGame()
+        {
+            Debug.Log("End");
+            Indicator.SetColor("_EmissionColor", new Color(0f, 255f, 0f));
         }
 
         public override void LaunchGame()
