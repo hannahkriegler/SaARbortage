0e9a5a69c6bd8cd9e5e364066a5d8d9c9054f16f
Niklas Braun <NiklasBraun93@googlemail.com>
2021-06-21 17:26:38 +0200

    short-memory-minigame 0.7
    implemented core functions. Needs tweeking visually.

--- UnityProject/Assets/Scripts/Games/ShortTimeMemoryGame.cs
+++ UnityProject/Assets/Scripts/Games/ShortTimeMemoryGame.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,15 +18,26 @@ namespace SaARbotage
         private bool _playing = false;
 
         public Image[] imageField;
-        private int _last;
+        private bool[] _rdyCheck;
 
         private int[] _solution;
         private int _counter = 0;
         private List<int> _input;
 
+        public Color signalColor;
+        private Color _initialCol;
+        public Color failColor;
+        public Color winColor;
+
         private void Awake()
         {
             _solution = new int[solutionLength];
+            _rdyCheck = new bool[solutionLength];
+            for (int i = 0; i < _rdyCheck.Length; i++)
+            {
+                _rdyCheck[i] = false;
+            }
+
         }
 
         // Update is called once per frame
@@ -33,20 +45,20 @@ namespace SaARbotage
         {
             if (!_playing && !_isstarted && rounds > 0)
             {
-                for(int i = 0; i< solutionLength; i++)
-                {
-                    int num = (int) UnityEngine.Random.Range(0, imageField.Length);
-                    _solution[i] = num;
-                    imageField[_solution[i]].color = Color.blue;
-                    Debug.Log("the " + i.ToString() + " value is: " + num.ToString());
-                }
+                _initialCol = imageField[0].color;
+                SetUpRound();
                 _isstarted = true;
 
             }
+
             if (!_playing && rounds > 0)
-            {   // hier coroutinen für den Farbwechsel. Wenn alle Coroutinen done sind, dann darf man spielen!
-                
-                _playing = true;
+            {
+                bool rdy = true;
+                foreach(bool a in _rdyCheck)
+                {
+                    rdy = a && rdy;                       
+                }
+                if (rdy) _playing = true;
             }
 
             if (_playing && rounds > 0)
@@ -56,7 +68,16 @@ namespace SaARbotage
                     Debug.Log("ROUND SUCCEESSS!!!");
                     _isstarted = false;
                     _playing = false;
+                    _counter = 0;
                     rounds--;
+                    for (int i = 0; i <_rdyCheck.Length; i++)
+                    {
+                        _rdyCheck[i] = false;
+                    }
+                    foreach(Image im in imageField)
+                    {
+                        im.color = _initialCol;
+                    }
                         }
                 if (rounds <= 0)
                 {
@@ -67,6 +88,21 @@ namespace SaARbotage
 
         }
 
+        private void SetUpRound()
+        {
+            for (int i = 0; i < solutionLength; i++)
+            {
+                int num = (int)UnityEngine.Random.Range(0, imageField.Length);
+                _solution[i] = num;
+                //Start Color animation which goes to Color X and back to standard.
+                StartCoroutine(Colorchange((float) i, imageField[num] ));
+                //imageField[_solution[i]].color = signalColor;
+                Debug.Log("the " + i.ToString() + " value is: " + num.ToString());
+            }
+        }
+
+
+
         public void InputField(int numb)
         {
             if (!_playing) return;
@@ -74,12 +110,34 @@ namespace SaARbotage
             {
                 Debug.Log("Correct");
                 _counter++;
+                imageField[numb].color = winColor;
                 //this.CrossFadeColor(Color.green, speed, false, false);
 
             }
             else {
                 Debug.Log("false");
-                life -= 1; }
+                life -= 1;
+                imageField[numb].color = failColor;
+            }
+
+        }
+
+        IEnumerator Colorchange(float order, Image im)
+        {
+            //TODO: Mehr Richtung LERP undso.. Ist mir noch zu hart von der transition.
+            float timeelapsed = 0f;
+            while(timeelapsed < order) {
+                timeelapsed += Time.deltaTime;
+                yield return null;
+            }
+            while (timeelapsed >= order && timeelapsed < order+animTime)
+            {
+                im.color = signalColor;
+                timeelapsed += Time.deltaTime;
+                yield return null;
+            }
+            im.color = _initialCol;
+            _rdyCheck[(int)order] = true;
         }
     }
 }
